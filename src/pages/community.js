078ff13269ebe3c1762b2ab1617d4ca66a5e5356
Jiawei-Zhao-728 <zhaoranjiawei@gmail.com>
2022-10-28 16:57:52 -0400

    adding community info

--- src/pages/community.js
+++ src/pages/community.js
@@ -4,16 +4,14 @@ const Community = () => {
   return (
     <div>
       <div class="container">
-      <div class="row align-items-center my-5">
+        <div class="row align-items-center my-5">
       <div class="row align-items-center my-5"> </div>
       <h1 class="font-weight-light">Welcome to the NerF or Nothing Community</h1>
       <div class="row align-items-center my-3"></div>
       <h3 class="font-weight-light">This is a thriving community where individuals can share their cool NerF renders with others in the community.</h3>
 
-          </div>
-          </div>
-
-
+        </div>
+      </div>
       
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,12 @@ const Home = () => {
 
       <div class="container">
       <div class="row align-items-center my-5">
+      <div class="row align-items-center my-5"> </div>
       <h1 class="font-weight-light">NerF or Nothing</h1>
+      <div class="row align-items-center my-3"></div>
+      <h3 class="font-weight-light"> The next generation object rendering technology utilizing the Neural Radiance Field technology. </h3>
           </div>
-          </div>
+        </div>
         </div>
   );
 };
