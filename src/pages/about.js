98d87c233b6fe862933a70883ee24daa4bfc1d02
Jiawei-Zhao-728 <zhaoranjiawei@gmail.com>
2022-11-07 09:09:43 -0500

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,18 +12,18 @@ const About = () => {
 
       <h3>General Overview</h3>
 
-      <h>
+      <h5>
       In general, the project consists of the frontend website that takes the
     input video from user, a backend server that is implemented with TensorF and
     Colmap that process the video and creates the 3D fly through of the object,
     and a web server that is builded with flask that serves as a database to
     connect and pass through data between the frontend and backend. Lastly, the
     connections between all the branches will the a set of HTTP requests.
-      </h>
+      </h5>
 
       <div class="row align-items-center my-3"></div>
 
-      <h>
+      <h5>
       In the beginning, The user uploads a video to the frontend website, that
     video will then be send to the Web server, more specifically the MongoDB
     database. Then, the Flask web server will send the input video to RabbitMQ,
@@ -37,7 +37,7 @@ const About = () => {
     object will be send out of the RabbitMQ, and back to Web server, sorted in
     the MongoDB database ready for the Front-end VueJS to access, and displayed
     in the page.
-      </h>
+      </h5>
 
       <div class="row align-items-center my-3"></div>
       <div class="row align-items-center my-3"></div>
@@ -48,14 +48,14 @@ const About = () => {
       NerF Technology via Colmap and Structure for motion and implemented by TencerRF
       </h4>
 
-      <h>    
+      <h5>    
         In a general, NerF is a neural network that generates novel views of complex
     3D scenes with 2d images and their coordinates. It works by taking input
     images representing a scene and interpolating between them to render one
     complete scene. A NeRF uses a sparse set of input views to optimize a
     continuous volumetric scene function. The result of this optimization is the
     ability to produce novel views of a complex scene.
-    </h>
+    </h5>
 
     <div class="row align-items-center my-3"></div>
     <div class="row align-items-center my-3"></div>
@@ -66,7 +66,7 @@ const About = () => {
     Flask with Mongdb and Rabbitmq
     </h4>
 
-    <h>    
+    <h5>    
       There are three components that makes up the web server branch, Flask web
     server, MongoDB database. First, the Flask web server serves as a connection
     point that manages messages transmitting between the frontend and the
@@ -74,20 +74,20 @@ const About = () => {
     for motion queue, the flask web server will constantly get information about
     the time remaining for completion and feet that to the frontend to let the
     users know the time remaining.
-    </h>
+    </h5>
     <div class="row align-items-center my-3"></div>
-    <h>
+    <h5>
       Second, MongoDB is a asynchronous messenger that hoses the structure for
     motion queue and NerF queue for backend video processing; it’s used to
     manage the input video is first send to the SFM queue for processing, and
     when it’s done, it’s send to the NerF queue for further processing.
-    </h>
+    </h5>
     <div class="row align-items-center my-3"></div>
-    <h>
+    <h5>
       Lastly, the MongoDB database servers as a place to hold the input videos and
     output fly through videos, as such implementation is easier for the frontend
     and backend to access the needed resources.
-    </h>
+    </h5>
 
 
     <div class="row align-items-center my-3"></div>
@@ -99,9 +99,9 @@ const About = () => {
     React.js & Bootstrap
     </h4>
 
-    <h>    
+    <h5>    
       The app is currently builded with React.js with the Bootstrap plugin. 
-    </h>
+    </h5>
 
       </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import button from 'react-bootstrap/Button';
   
 const Home = () => {
   return (
@@ -7,9 +8,15 @@ const Home = () => {
       <div class="container">
       <div class="row align-items-center my-5">
       <div class="row align-items-center my-5"> </div>
-      <h1 class="font-weight-light">NerF or Nothing</h1>
+      <h1 class="font-weight-light">NeRF or Nothing</h1>
       <div class="row align-items-center my-3"></div>
       <h3 class="font-weight-light"> The next generation object rendering technology utilizing the Neural Radiance Field technology. </h3>
+
+
+      <div class="row align-items-center my-3"></div>
+
+      <button type = "button" class = "btn btn-success">Submit Video</button>{' '}
+
           </div>
         </div>
         </div>
