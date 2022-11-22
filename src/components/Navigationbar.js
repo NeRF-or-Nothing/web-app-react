eff305d2f5363a3ae56d1b11969acb8de0c2be18
Jiawei-Zhao-728 <zhaoranjiawei@gmail.com>
2022-11-22 16:18:53 -0500

    added logo in the other pages

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,22 +1,27 @@
-import {Nav, Navbar, NavLink} from "react-bootstrap";
-import {Link} from "react-router-dom"
-import React from 'react';
+import { Nav, Navbar, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const Navigationbar = () => {
-    return (
-        <Navbar collapseOnSelect expand = "lg" bg = "dark" variant = "dark" >
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Collapse id="navbarScroll">
+        <div class="container">
+          <Nav>
+            <NavLink eventKey="1" as={Link} to="">
+              Home
+            </NavLink>
+            <NavLink eventKey="3" as={Link} to="/about">
+              About
+            </NavLink>
+            <NavLink eventKey="2" as={Link} to="/community">
+              Community
+            </NavLink>
+          </Nav>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-            <Navbar.Collapse id = "navbarScroll">
-            <div class="container">
-                <Nav>
-                    <NavLink eventKey="1"  as = {Link} to = "">Home</NavLink>
-                    <NavLink eventKey="3" as = {Link} to = "/about">About</NavLink>
-                    <NavLink eventKey="2" as = {Link} to = "/community">Community</NavLink>
-                </Nav>
-            </div>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-}
-
-export default Navigationbar; 
+export default Navigationbar;
--- src/pages/community.js
+++ src/pages/community.js
@@ -1,20 +1,30 @@
 import React from "react";
-  
+
 const Community = () => {
   return (
     <div>
+      <div>
+        <img
+          src="./logo.png"
+          alt="N logo"
+          style={{ width: 500, height: 300 }}
+        />
+      </div>
       <div class="container">
         <div class="row align-items-center my-5">
-      <div class="row align-items-center my-5"> </div>
-      <h1 class="font-weight-light">Welcome to the NerF or Nothing Community</h1>
-      <div class="row align-items-center my-3"></div>
-      <h3 class="font-weight-light">This is a thriving community where individuals can share their cool NerF renders with others in the community.</h3>
-
+          <div class="row align-items-center my-5"> </div>
+          <h1 class="font-weight-light">
+            Welcome to the NerF or Nothing Community
+          </h1>
+          <div class="row align-items-center my-3"></div>
+          <h3 class="font-weight-light">
+            This is a thriving community where individuals can share their cool
+            NerF renders with others in the community.
+          </h3>
         </div>
       </div>
-      
     </div>
   );
 };
-  
+
 export default Community;
