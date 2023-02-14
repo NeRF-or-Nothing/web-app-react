342ae52ac0973c556ff5077b80a465ae76990426
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-02-14 17:41:32 -0500

    SignIn and LogoIn

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React,{useState} from 'react';
+import "./Signup.css";
+import logo from "./pure_logo.png";
 
-export default function Signup(){
-    return(
-        <div>
-            <h2>Blank text</h2>
-        </div>
-    )
+function Signup() {
+  
+  return (   
+    <body>
+      <div class="logo">
+        <img className="nerfLogo" src={logo}/>
+      </div>
+      <div class="main">
+        <p class="sign">
+          <div>Create a new account</div> 
+          <div class="login-div"><span class="AAccount"> Already have an account? <a href="./login">Sign Up</a> </span> </div>
+        </p>
+          <form class="form1">
+            <input class="Name " type="text" align="center" placeholder="Name"/>
+            <input class="un " type="text" align="center" placeholder="Username"/>
+            <input class="email " type="text" align="center" placeholder="Email"/>
+            <input class="pass" type="password" align="center" placeholder="Password"/>
+            <a class="submit" align="center">Sign in</a>  
+          </form>
+      </div>
+    </body>
+  );
 }
+
+export default Signup;
