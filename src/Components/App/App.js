b45129fdf7ee9d187eb25530bcd909815462bde9
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-02-14 17:44:12 -0500

    added transparent git logo, footer wip

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,21 +6,23 @@ import Login from "../Login/Login";
 import Signup from "../Signup/Signup";
 import NavBar from "../NavbarLink/NavbarLink";
 import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Footer from "../Footer/Footer"
 
 function App(){
   return(
     //<NavBar/> //for testing purposes, Tashi
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home/>}></Route>
-        <Route exact path="/Community" element={<Community/>}/>
-        <Route exact path="/Home" element={<Home/>}/>
-        <Route exact path="/Loading" element={<Loading/>}/>
-        <Route exact path="/Signup" element={<Signup/>}/>
-        <Route exact path="/Login" element={<Login/>}/>
-        <Route exact path="/FullRendered" element={<FullRendered/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Footer/> //for testing purposes, 
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route exact path="/" element={<Home/>}></Route>
+    //     <Route exact path="/Community" element={<Community/>}/>
+    //     <Route exact path="/Home" element={<Home/>}/>
+    //     <Route exact path="/Loading" element={<Loading/>}/>
+    //     <Route exact path="/Signup" element={<Signup/>}/>
+    //     <Route exact path="/Login" element={<Login/>}/>
+    //     <Route exact path="/FullRendered" element={<FullRendered/>}/>
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
