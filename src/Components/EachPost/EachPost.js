18f6c04370af412e5f26c99cf41e7cb8e1c7a6fc
Paul Gaddy <35400575+notpaulgaddy@users.noreply.github.com>
2023-02-14 16:51:56 -0500

    Add files via upload
    Base of each post component

---
+++ src/Components/EachPost/EachPost.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./EachPost.css";
+
+export default function EachPost(){
+    return(
+        <div>
+            <hr/>
+            <div className="picNameUsername">
+                <div className="imageSide">
+                    <img src="https://pbs.twimg.com/media/EgdP6MfWAAAH0gu.jpg" className="eachPfp"/>
+                </div>
+                <div className="nameUsernameSide">
+                    <div className="theName">
+                        <h1>Bob Sugarhand</h1>
+                    </div>
+                    <div className="theUsername">
+                        <h2>@cheeseburgerBob</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="postTextImage">
+                <h3>This is my render, pretty cool right!</h3>
+                <img src="https://static.chaos.com/images/assets/000/011/316/full_width_original/Vision_video_1280x720.jpg?1604673750"/>
+            </div>
+        </div>
+    )
+}
