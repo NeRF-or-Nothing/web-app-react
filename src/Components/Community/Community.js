12794a29a61f356725ee625752ecebc60b800806
Paul Gaddy <35400575+notpaulgaddy@users.noreply.github.com>
2023-02-14 16:51:32 -0500

    Add files via upload
    Basics for community page

--- src/Components/Community/Community.js
+++ src/Components/Community/Community.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import "./Community.css";
 export default function Community(){
     return(
-        <div>
-            <h2>Blank text</h2>
+        <div className="mainDiv">
+            <div className="topPost">
+                <div className="topRow">
+                    <img src="https://pbs.twimg.com/media/EgdP6MfWAAAH0gu.jpg" className="yourProfilePic"/>
+                    <div className="largeBorderBox">
+                        <h2>Talk about your render here!</h2>
+                    </div>
+                </div>
+                <div className="bottomRow">
+                    <div className="photoBox">
+                        <h2>Photo</h2>
+                    </div>
+                    <div className="videoBox">
+                        <h2>Video</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
