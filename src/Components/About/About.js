f683cbee82008c9a03bb4fe235c02266ad39a20e
notpaulgaddy <paulanthony1201@hotmail.com>
2023-04-04 17:29:33 -0400

    Updated about page
    Page mostly done, just have to fix carousel sizing

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import exampleGif from "./example_output.gif";
 import explainDiagram from "./Full_Project.png";
+import box from "./box.png";
 import colmapPic from "./colmap.png";
 import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container';
@@ -20,26 +21,26 @@ export default function About() {
       <NavBar/>
       <section className="jumbotron text-center">
         <div className="container">
-          <div className="flex-container">
+          <div className="flex-container firstBox">
             <div className="flex-child left">
-              <h1 className="jumbotron-heading">"3D rendering <br />your videos made <div className="easy">easy.</div>"</h1>
+              <h1 className="jumbotron-heading">3D rendering <br />your videos made <div className="easy">easy.</div></h1>
             </div>
             <div className="flex-child right">
-              <img src="https://www.kindpng.com/picc/m/238-2380430_3d-rendering-services-logo-render-png-3d-transparent.png" />
+              <img src={box} />
             </div>
 
           </div>
           <div className="flex-container">
           <div className="flex-child left">
-          <div className="techText"><h2><span class="dot blueDot"></span>Our Technology</h2></div>
-          <p class="lead text-muted"><h2>NeRF consists of a website that
+          <div className="techText"><h3><span class="dot blueDot"></span>&nbsp;&nbsp;Our Technology</h3></div>
+          <p class="lead text-muted"><h3>NeRF consists of a website that
             receives video input from the
             user and creates a 3D fly through
             Of the object using TensorFlow
             and Colmap. This data is passed
             to our Flask server to connect
             All of our data and form a set of
-            HTTP requests.  </h2> </p>
+            HTTP requests.  </h3> </p>
           </div>
           <div className="flex-child right">
             <img src={exampleGif}/>
@@ -89,14 +90,15 @@ export default function About() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      <div className="beigeDiv">
       <div>
-      <div className="techText"><h2><span class="dot aquaDot"></span>How Does It Work?</h2></div>
+      <div className="techText"><h2><span class="dot aquaDot"></span>&nbsp;&nbsp;How Does It Work?</h2></div>
         <div className="miniPoints">
-          <h2><span class="dot aquaDot"></span>Uploaded video gets sent to our database</h2>
-          <h2><span class="dot aquaDot"></span>Flask server sends video to RabbitMQ
+          <h2><span class="dot aquaDot"></span>&nbsp;&nbsp;Uploaded video gets sent to our database</h2>
+          <h2><span class="dot aquaDot"></span>&nbsp;&nbsp;Flask server sends video to RabbitMQ
 for video processing</h2>
-          <h2><span class="dot aquaDot"></span>Video processed for 10-15 minutes</h2>
-          <h2><span class="dot aquaDot"></span>Our NeRF worker receives the processed
+          <h2><span class="dot aquaDot"></span>&nbsp;&nbsp;Video processed for 10-15 minutes</h2>
+          <h2><span class="dot aquaDot"></span>&nbsp;&nbsp;Our NeRF worker receives the processed
 video and creates a 3D fly object to then
 be displayed on the page </h2>
         </div>
@@ -104,18 +106,18 @@ be displayed on the page </h2>
         <img src={explainDiagram}/>
 
         <div>
-      <div className="techText"><h2><span class="dot orangeDot"></span>Back-End Structure and Framework</h2></div>
+      <div className="techText"><h2><span class="dot orangeDot"></span>&nbsp;&nbsp;Back-End Structure and Framework</h2></div>
         <div className="miniPoints">
-          <h2><span class="dot orangeDot"></span>NerF is a neural network that generates 
+          <h2><span class="dot orangeDot"></span>&nbsp;&nbsp;NerF is a neural network that generates 
 novel views of complex 3D scenes with 2d 
 images and their coordinates</h2>
-          <h2><span class="dot orangeDot"></span>We take input images representing a 
+          <h2><span class="dot orangeDot"></span>&nbsp;&nbsp;We take input images representing a 
 scene and interpolating between them 
 to render one complete scene</h2>
-          <h2><span class="dot orangeDot"></span>NeRF uses a sparse set of input views to 
+          <h2><span class="dot orangeDot"></span>&nbsp;&nbsp;NeRF uses a sparse set of input views to 
 optimize a continuous volumetric scene 
 function</h2>
-          <h2><span class="dot orangeDot"></span>The result of this optimization is the ability
+          <h2><span class="dot orangeDot"></span>&nbsp;&nbsp;The result of this optimization is the ability
  to produce novel views of a complex 
 scene. </h2>
         </div>
@@ -123,21 +125,22 @@ scene. </h2>
         <img src={colmapPic}/>
 
         <div>
-      <div className="techText"><h2><span class="dot pinkDot"></span>Webserver Structure and Framework</h2></div>
+      <div className="techText"><h2><span class="dot pinkDot"></span>&nbsp;&nbsp;Webserver Structure and Framework</h2></div>
         
         <div className="miniPoints">
-          <h2><span class="dot pinkDot"></span>Flask web server serves as a connection 
+          <h2><span class="dot pinkDot"></span>&nbsp;&nbsp;Flask web server serves as a connection 
 point that manages messages transmitting 
 between the frontend and the backend</h2>
-          <h2><span class="dot pinkDot"></span>MongoDB is a asynchronous messenger 
+          <h2><span class="dot pinkDot"></span>&nbsp;&nbsp;MongoDB is a asynchronous messenger 
 that hoses the structure for motion queue 
 and NerF queue for backend video 
 processing.</h2>
-          <h2><span class="dot pinkDot"></span>MongoDB database servers as a place to 
+          <h2><span class="dot pinkDot"></span>&nbsp;&nbsp;MongoDB database servers as a place to 
 hold the input videos and output fly through 
 videos, as such implementation is easier for 
 the frontend and backend to access the 
 needed resources.</h2>
+        </div>
         </div>
         </div>
 
--- src/Components/EachPost/EachPost.js
+++ src/Components/EachPost/EachPost.js
@@ -1,36 +1,16 @@
 import React from "react";
 import "./EachPost.css";
 
-export default function EachPost(){
-    return(
-        // <div className="eachBox">
-        //     <hr/>
-        //     <div className="picNameUsername">
-        //         <div className="imageSide">
-        //             <img src="https://pbs.twimg.com/media/EgdP6MfWAAAH0gu.jpg" className="eachPfp"/>
-        //         </div>
-        //         <div className="nameUsernameSide">
-        //             <div className="theName">
-        //                 <h1>Bob Sugarhand</h1>
-        //             </div>
-        //             <div className="theUsername">
-        //                 <h2>@cheeseburgerBob</h2>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <div className="postTextImage">
-        //         <h3>This is my render, pretty cool right!</h3>
-        //         <img src="https://static.chaos.com/images/assets/000/011/316/full_width_original/Vision_video_1280x720.jpg?1604673750"/>
-        //     </div>
-        // </div>
-        <div class="card">
-  <div class="card-header">
-    @Username
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Here is my post.</h5>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqxy5v16LUu2-LsRUaypLBwZQEaxMzDl678oYscXZWug&usqp=CAU&ec=48665699"/>
-  </div>
-</div>
-    )
+export default function EachPost() {
+  return (
+    <div class="card">
+      <div class="card-header">
+        @Username
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Here is my post.</h5>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqxy5v16LUu2-LsRUaypLBwZQEaxMzDl678oYscXZWug&usqp=CAU&ec=48665699" />
+      </div>
+    </div>
+  )
 }
