fd51858ce8acae3ada0d8c57a503f99b91f28222
= <=>
2023-09-29 17:00:10 -0400

    Cleaned up Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,35 +12,50 @@ export default function Home(){
       };
     return(
         <div>
-        <NavBar/>
-        <div>
-            <div className="topBox">
-                <h3 className="nerfText">NeRF or Nothing</h3>
-                <div className="topNerfPic">
-                    <img className="nerfLogo" src={logo}/>
+
+            <NavBar/>
+
+            {/* start of inner div */}
+            <div>
+
+                {/* start of topBox div */}
+                <div className="topBox">
+                    <h3 className="nerfText">NeRF or Nothing</h3>
+
+                    {/* start of topNerfPic div */}
+                    <div className="topNerfPic">
+                        <img className="nerfLogo" src={logo}/>
+                    </div>
+
+                    <h2 className="generationText" style={{paddingBottom:"4vh"}}>The next generation object rendering technology
+                        utilizing the Neural Radiance Field technology.
+                    </h2>
                 </div>
-                <h2 className="generationText" style={{paddingBottom:"4vh"}}>The next generation object rendering technology
-                    utilizing the Neural Radiance Field technology.
-                </h2>
-            </div>
-            <div className="yellowDiv">
-                <div className="innerDiv" style={{textAlign:"center"}}>
-                <h2 style={{paddingTop:"4vh"}}>Drag a file of the video you <br/> want to be rendered
-                    and watch <br/> the magic happen.
-                </h2>
-                <h4 style={{paddingTop:"3vh"}}>*Video file must be in .mp4 or mov format.</h4>
-                <FileUploader
-        multiple={true}
-        handleChange={handleChange}
-        name="file"
-        types={fileTypes}
-        style={{margin:"auto"}}
-      />
-      <p>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
+
+                {/* start of yellowDiv */}
+                <div className="yellowDiv">
+
+                    {/* start of innerDiv */}
+                    <div className="innerDiv" style={{textAlign:"center"}}>
+
+                        <h2 style={{paddingTop:"4vh"}}>Drag a file of the video you <br/> want to be rendered and watch <br/> the magic happen.
+                        </h2>
+
+                        <h4 style={{paddingTop:"3vh"}}>*Video file must be in .mp4 or mov format.</h4>
+                        <FileUploader
+                            multiple={true}
+                            handleChange={handleChange}
+                            name="file"
+                            types={fileTypes}
+                            style={{margin:"auto"}}
+                        />
+                        <p>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
+                    </div>
+
                 </div>
+
             </div>
-        </div>
-        <Footer/>
+            <Footer/>
         </div>
     )
 }
