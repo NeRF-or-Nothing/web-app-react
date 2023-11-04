683e4f209df0a2812a77ac0c4e3a43254fbf12c5
Gabe C <113395303+GabeVC@users.noreply.github.com>
2023-11-03 22:04:08 -0400

    Update Login.js for backend
    Tried to make it compatible with backend, will continue to update and finalize

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,26 @@
 import React,{useState} from 'react';
 import "./Login.css";
 import logo from "./pure_logo.png";
+import axios from 'axios';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   
+  const signIn = async () => {
+  try {
+    const response = await axios.post("http://localhost:3000", {
+      username,
+      password,
+  });
+
+  console.log('Response from the server:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
   return (   
     <body>
       <div class="logo">
@@ -15,9 +32,9 @@ function Login() {
           <div class="login-div"><span class="noAccount"> Don't have an account? <a href="./Signup">Sign Up</a> </span> </div>
         </p>
           <form class="form1">
-            <input class="un " type="text" align="center" name="username" placeholder="Username"/>
-            <input class="pass" type="password" align="center" nm="password" placeholder="Password"/>
-            <a class="submit" align="center">Sign in</a>  
+            <input class="un " type="text" align="center" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input class="pass" type="password" align="center" nm="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <a class="submit" onCLick={signIn} align="center">Sign in</a>  
           </form>
       </div>
     </body>
